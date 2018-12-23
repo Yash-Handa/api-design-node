6fdeafafb45c9337a59ba7a1a6437c06dbf11492
Yash Handa <yashhanda7@yahoo.com>
2018-12-24 03:01:05 +0530

    Solution for 1st exercise

--- server.js
+++ server.js
@@ -2,4 +2,31 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+const express = require('express');
+const fs = require('fs');
+const { join } = require('path');
+const app = express();
+
+var jsonData = {
+  count: 12,
+  message: 'hey'
+};
+
+app.get('/', (req, res) => {
+  fs.readFile(join(__dirname, 'index.html'), (err, buffer) => {
+    if (err) res.status(500).send(err);
+    const html = buffer.toString();
+    res.status(200).send(html);
+  });
+
+    // node built in modules support callbacks only (probably)
+    // .then((buffer) => {
+    //   const html = buffer.toString();
+    //   res.status(200).send(html);
+    // }).catch((err) => {
+    //   res.status(500).send(err);
+    // });
+});
+app.get('/data', (req, res) => res.status(200).json(jsonData));
+
+app.listen(3000, () => console.log('server running on port 3000'));
